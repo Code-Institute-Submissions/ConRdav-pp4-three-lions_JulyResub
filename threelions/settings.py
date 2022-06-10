d5b155362cec36ed4ba9a1c7bc35abb5f0609b30
ConRdav <connor@redwoodglade.com>
2022-06-10 10:17:09 +0000

    Add whitenoise to settings.py

--- threelions/settings.py
+++ threelions/settings.py
@@ -84,6 +84,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'threelions.urls'
