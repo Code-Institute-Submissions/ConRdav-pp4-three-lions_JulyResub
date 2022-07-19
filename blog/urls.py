707c1f63c5c86ff233cc83c188091499258c6140
ConRdav <connor@redwoodglade.com>
2022-07-19 18:33:10 +0000

    Update create_post.html, edit_posts.html, user_posts.html and user_profile.html to improve UI

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('<str:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<str:slug>', views.PostLike.as_view(), name='post_like'),
     path('create_post', views.CreatePost.as_view(), name='create_post'),
     path('user_posts', views.user_posts, name='user_posts'),
     path('edit/<post_id>', views.edit_post, name='edit_post'),
